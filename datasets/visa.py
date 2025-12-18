56f5f46d77017417cde3e4cfa4dd4ef9b0b1206c
guanshantingbai <2428050771@qq.com>
2025-12-18 17:42:33 +0800

    timecounter0

--- datasets/visa.py
+++ datasets/visa.py
@@ -6,8 +6,8 @@ visa_classes = ['candle', 'capsules', 'cashew', 'chewinggum',
                    'fryum', 'macaroni1', 'macaroni2',
                 'pcb1', 'pcb2', 'pcb3','pcb4', 'pipe_fryum']
 
-# VISA_DIR = '/data2/grj/pro_visa/1cls/'
-VISA_DIR = '/home/zju/datasets/pro_visa/1cls/'
+VISA_DIR = '/data2/grj/pro_visa/1cls/'
+# VISA_DIR = '/home/zju/datasets/pro_visa/1cls/'
 
 
 def load_visa(category, k_shot):
